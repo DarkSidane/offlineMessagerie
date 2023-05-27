1765e426725c5f966e0bd60df6d9ba2db205b505
Amine Boujemaaoui <92468393+amine-boujemaaoui@users.noreply.github.com>
2023-05-27 12:26:51 +0100

    Fix main.c

--- main.c
+++ main.c
@@ -4,97 +4,81 @@
 #define TRUE 1
 #define FALSE 0
 
-//Taille max d'une chaîne de caractères
+// Taille max d'une chaîne de caractères
 #define S_MAX 256
-char** decoupe_mots(char* buf)
+char **decoupe_mots(char *buf)
 {
-	//Prend un mot en argument et renvoie les mots contenus dedans
+	// Prend un mot en argument et renvoie les mots contenus dedans
 	char delim[] = " ";
-	char** com;
+	char **com;
 	com = malloc(2 * S_MAX * sizeof(char));
 	char *ptr = strtok(buf, delim);
 
 	int i = 0;
-	while (ptr != NULL) {
+	while (ptr != NULL)
+	{
 		com[i] = ptr;
 		i++;
 		ptr = strtok(NULL, delim);
 	}
-
-	for (size_t i = 0; i < 2; i++) {
-		printf("%s - ", com[i]);
-	}
-	// dec
 	return com;
 }
 int interp_commande(char *commande)
 {
 	/*Interprète les commandes et exécute la fonction appropriée.
-	 * Renvoie 1 si la commande est différente de q, 0 sinon. 
+	 * Renvoie 1 si la commande est différente de q, 0 sinon.
 	 * La fonction découpe_mots servira à découper la commande dans les cas où il y a des arguments (par exemple : la commande p).*/
 	char **buf = decoupe_mots(commande);
 	char nom[S_MAX];
 	int enregistre = TRUE;
-	switch (buf[0][0]) 
-	{	
-		case 'e' :
-			// Si on veut enregistrer le nom d'utilisateur 
+	if (strlen(buf[0]) > 1) printf("Commande non recconu\n"); 
+	else {
+		switch (buf[0][0])
+		{
+		case 'e':
+			// Si on veut enregistrer le nom d'utilisateur
 			if (buf[1] == 0) printf("Vous devez renseigner un nom d'utilisateur");
-			else 
-			{
-				
+			else {
 				printf("Votre nom d'utilisateur est %s\n", buf[1]);
 				printf("Enregistrement en cours...\n");
 				enregistre = TRUE;
 			}
 			break;
-		case 'p' : 
-			if (enregistre==TRUE)
+		case 'p':
+			if (enregistre == TRUE)
 				printf("Ouverture de la fenêtre de dialogue\n");
-			else 
+			else
 				printf("Vous n'êtes pas enregistré ! \n");
 			break;
-		case 'q' : 
+		case 'q':
 			printf("Au revoir.\n");
-			return 1;
+			return FALSE;
 			break;
-		default : 
+		default:
 			printf("Veuillez écrire une lettre entre e, p ou q\n");
 			break;
+		}
+		printf("\n===================================\n");
 	}
-	printf("\n===================================\n");
-	return 0;
+	return TRUE;
+	
 }
 
 int main(int argc, char **argv)
 {
 	int running = TRUE;
-	int cmd = -1; // Cette variable sert à stocker commande rentré l'indice vaut 0 si l'utilsateur rentre e, 1 si p, q si 2, 3 sinon) 
 	char buf[S_MAX];
 
-	/*
-	while (running == TRUE) {
-		// Boucle d'execution de notre programme
-		char *commande;
-		printf("Entrez une commande (e, p, q) \n");
-		fgets(buf, S_MAX, stdin);
-		cmd = interp_commande(buf);
-		if (cmd == 1)
-			running = FALSE	; */
-
-	char *line = NULL;
-	size_t linecap = 0;
-	size_t linelen;
-
-	// while ((linelen = getline(&line, &linecap, stdin)) > 0) {
-	while (linelen = fgets(buf, S_MAX, stdin) != NULL) {
+	while (running == TRUE)
+	{
+		while (fgets(buf, S_MAX, stdin) != NULL)
+		{
 			buf[strcspn(buf, "\n")] = 0;
 			break;
+		}
+		fflush(stdin);
+		running = interp_commande(buf);
 	}
-
-	// delimitation
-	interp_commande(buf);
 
 	return EXIT_SUCCESS;
 }
-
